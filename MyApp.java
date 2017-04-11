09909cc139737d15fecd02f9d7d8cec613aadc65
dirval <dirval@debian>
2017-04-11 16:03:26 +0300

    java b2 v3

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,6 @@
 class main {
     public static void main(String[] args) {
     	Product product = new Product("Edam", 3.3, 120);
-
       product.changePrice();
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
@@ -24,12 +23,6 @@ class main {
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
-
-
-
-
-
-  
 
   public void changePrice(){
     this.price = 5;
